96caafdf2db74063b026385a948cfb8d6ae406a4
DonatasNoreika <donoras@gmail.com>
2026-07-30 18:23:16 +0300

    2: +Sukurti visus modelius pagal nurodytą programos DB struktūrą

--- mysite/autoservice/models.py
+++ mysite/autoservice/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
+
 # Create your models here.
+class Service(models.Model):
+    name = models.CharField()
+    price = models.DecimalField(decimal_places=2, max_digits=4)
+
+    def __str__(self):
+        return self.name
+
+
+class Car(models.Model):
+    make = models.CharField()
+    model = models.CharField()
+    license_plate = models.CharField(max_length=10)
+    vin_code = models.CharField(max_length=20)
+    client_name = models.CharField()
+
+    def __str__(self):
+        return f"{self.make} {self.model} ({self.license_plate})"
+
+
+class Order(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    car = models.ForeignKey(to="Car", on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.car.make} {self.car.model} ({self.date})"
+
+
+class OrderLine(models.Model):
+    order = models.ForeignKey(to="Order", on_delete=models.CASCADE)
+    service = models.ForeignKey(to="Service", on_delete=models.SET_NULL, null=True, blank=True)
+    qty = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.service} - {self.qty}"
