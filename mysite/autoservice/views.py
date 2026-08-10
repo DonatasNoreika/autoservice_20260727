71378a097f1a24d0387d2b3322290b45baf1c88e
DonatasNoreika <donoras@gmail.com>
2026-08-10 18:07:35 +0300

    5: +Sukurti puslapį (per funkciją views faile), pvz. autoservice/automobiliai, kuriame būtų atvaizduoti visi servise užregistruoti automobiliai.

--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -12,3 +12,9 @@ def index(request):
     }
     return render(request, template_name="index.html", context=context)
 
+
+def cars(request):
+    context = {
+        "cars": Car.objects.all(),
+    }
+    return render(request, template_name="cars.html", context=context)
